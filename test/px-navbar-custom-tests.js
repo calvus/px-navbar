6e9b349838f943f470531567e5846434cb31c0f4
Jonnie Spratley <jonnie.spratley@ge.com>
2016-05-11 15:07:17 -0700

    fix: updated tests

--- test/px-navbar-custom-tests.js
+++ test/px-navbar-custom-tests.js
@@ -1,58 +1,58 @@
 // This is the wrapper for custom tests, called upon web components ready state
 function runCustomTests() {
   // Place any setup steps like variable declaration and initialization here
-  suite('<px-navbar>', function() {
+  suite('<px-navbar>', function () {
     var element = document.getElementById('fixture');
 
-    test('it should have render', function() {
+    test('it should have render', function () {
       assert.ok(element);
     });
 
-    test('it should have title', function() {
+    test('it should have title', function () {
       assert(element.title);
       assert.equal(element.title, 'WCT');
     });
 
-    test('it should have sub-title', function() {
+    test('it should have sub-title', function () {
       element.subtitle = 'Unit Tests';
       assert.equal(element.subtitle, 'Unit Tests');
     });
 
-    test('it should change title to current page title.', function() {
+    test('it should change title to current page title.', function () {
       element.title = 'Test Title';
       assert.equal(element.title, 'Test Title');
     });
 
 
-    suite('Theme', function() {
-      test('is should change theme to primary', function() {
+    suite('Theme', function () {
+      test('is should change theme to primary', function () {
         element.theme = 'primary';
         assert.equal(element.theme, 'primary');
       });
 
-      test('is should change theme to chrome', function() {
+      test('is should change theme to chrome', function () {
         element.theme = 'chrome';
         assert.equal(element.theme, 'chrome');
       });
 
-      test('is should change theme to dark', function() {
+      test('is should change theme to dark', function () {
         element.theme = 'dark';
         assert.equal(element.theme, 'dark');
       });
 
     });
 
-    suite('Back Button', function() {
-      test('is not visible by default', function() {
+    suite('Back Button', function () {
+      test('is not visible by default', function () {
         assert.equal(element.back, false);
       });
 
-      test('it should be visible', function() {
+      test('it should be visible', function () {
         element.back = true;
         assert.equal(element.back, true);
       });
 
-      test('it should change back button text', function() {
+      test('it should change back button text', function () {
         element.backBtnLabel = 'Back to Settings';
         assert.equal(element.backBtnLabel, 'Back to Settings');
       });
